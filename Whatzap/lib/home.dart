820f890cb5111514d5592bcbd385efd806fd9246
Samhebert <samuelheber99ori@gmail.com>
2023-03-12 09:55:17 -0300

    Correção do problema das rotas

--- Whatzap/lib/home.dart
+++ Whatzap/lib/home.dart
@@ -25,10 +25,8 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
 
   @override
   void initState() {
-    super.initState();
-
     _recuperarDadosUsuario();
-
+    super.initState();
     _tabController = TabController(length: 2, vsync: this);
   }
 
@@ -42,52 +40,52 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
         break;
     }
   }
-    _deslogaUsuario() async {
-      FirebaseAuth auth = FirebaseAuth.instance;
-      await auth.signOut();
+  _deslogaUsuario() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    await auth.signOut();
 
-      Navigator.pushReplacement(
+    Navigator.pushReplacement(
         context, MaterialPageRoute(builder: (context) => Login())
-      );
-    }
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-            title: Text("WhatsApp"),
-            backgroundColor: Colors.green,
-            foregroundColor: Colors.white,
-            bottom: TabBar(
-              indicatorWeight: 4,
-              labelStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              controller: _tabController,
-              indicatorColor: Colors.white,
-              tabs: <Widget>[
-                Tab(
-                  text: "Conversas",
-                ),
-                Tab(
-                  text: "Contatos",
-                )
-              ],
-            ),
-            actions: <Widget>[
-              PopupMenuButton<String>(
-                onSelected: _escolhaMenuItem,
-                itemBuilder: (context) {
-                  return itensMenu.map((String item) {
-                    return PopupMenuItem<String>(
-                      value: item,
-                      child: Text(item),
-                    );
-                  }).toList();
-                },
-              )
-            ]),
-        body: TabBarView(
-          controller: _tabController,
-          children: <Widget>[AbaConversas(), AbaContatos()],
-        ),
-      );
-    }
+    );
   }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("WhatsApp"),
+          backgroundColor: Colors.green,
+          foregroundColor: Colors.white,
+          bottom: TabBar(
+            indicatorWeight: 4,
+            labelStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            controller: _tabController,
+            indicatorColor: Colors.white,
+            tabs: <Widget>[
+              Tab(
+                text: "Conversas",
+              ),
+              Tab(
+                text: "Contatos",
+              )
+            ],
+          ),
+          actions: <Widget>[
+            PopupMenuButton<String>(
+              onSelected: _escolhaMenuItem,
+              itemBuilder: (context) {
+                return itensMenu.map((String item) {
+                  return PopupMenuItem<String>(
+                    value: item,
+                    child: Text(item),
+                  );
+                }).toList();
+              },
+            )
+          ]),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[AbaConversas(), AbaContatos()],
+      ),
+    );
+  }
+}
